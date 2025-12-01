6e10386dfa6d872afbb87602de2bdfdcb2efaf7b
stefanhall <stefan-hall@hotmail.com>
2025-12-01 20:10:38 +1300

    setup base stuff

---
+++ src/day01/index.ts
@@ -0,0 +1,9 @@
+
+
+export function solvePart1(input: string): number {
+    return 1;
+}
+
+export function solvePart2(input: string): number {
+    return 1;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+import {promises as fs} from "fs";
+import {solvePart1, solvePart2} from './day01/index.ts'
+
+async function load(filepath: string){
+    return await fs.readFile(filepath, "utf8")
+}
+
+
+load("../input.txt")
+    .then(input => solvePart1(input))
+    .then(console.log)
+    .catch(console.error)
+
+
+load("../input.txt")
+    .then(input => solvePart2(input))
+    .then(console.log)
+    .catch(console.error)
+
