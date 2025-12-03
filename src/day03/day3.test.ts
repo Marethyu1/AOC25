10799a32d1801ec6b65f4369bae175263ac0924d
stefanhall <stefan-hall@hotmail.com>
2025-12-03 21:14:07 +1300

    fixed

--- src/day03/day3.test.ts
+++ src/day03/day3.test.ts
@@ -2,20 +2,28 @@ import {findHighestDigitForWidth, maxLargeJoltage} from "./index.ts";
 import {test} from "node:test"
 import assert from "node:assert";
 
+const toBattery = (input: bigint) => input.toString().split("").map(x => parseInt(x));
+
+
 test('all', (t) => {
     t.test('can find highest digit [1,2,3]', () => {
-        let [startDigit, index] = findHighestDigitForWidth([1,2,3], 1)
+        let [startDigit, index] = findHighestDigitForWidth([1,2,3], 0)
         assert.strictEqual(startDigit, 3)
         assert.strictEqual(index, 2)
     });
 
+    test('can find highest digit [1,2,4,3]', () => {
+        let [startDigit, index] = findHighestDigitForWidth([1,2,4,3], 1)
+        assert.strictEqual(startDigit, 4)
+        assert.strictEqual(index, 2)
+    });
+
     t.test('can find highest digit [3,2,1]', () => {
-        let [startDigit, index] = findHighestDigitForWidth([3,2,1], 1)
+        let [startDigit, index] = findHighestDigitForWidth([3,2,1], 0)
         assert.strictEqual(startDigit, 3)
         assert.strictEqual(index, 0)
     });
 
-    const toBattery = (input: bigint) => input.toString().split("").map(x => parseInt(x));
 
     t.test('can find max joltage 987654321111111', () => {
         const input = toBattery(987654321111111n)
@@ -43,9 +51,13 @@ test('all', (t) => {
     })
 
     t.test('can find max big num', () => {
-        const input = toBattery(2231336232333236334112243332332341353232312512325222263432225332633322235632375232223315283333433363n)
+        let bigNum = 2231336232333236334112243332332341353232312512325222263432225332633322235632375232223315283333433363n;
+        const input = toBattery(bigNum)
+        let [startDigit, index] = findHighestDigitForWidth(input, 11)
+        assert.strictEqual(startDigit, 7)
         const joltage = maxLargeJoltage(input)
-        assert.strictEqual(joltage, 888911112111)
+        assert.strictEqual(joltage, 783333433363)
     })
 })
+
 
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -35,7 +35,7 @@ const arr = Array.from({ length: 9 }, (_, i) => 9 - i);
 export const findHighestDigitForWidth = (battery: battery, remainingDigits = 11) => {
     let startDigit = battery[0];
     let index = 0;
-    const y = battery.slice(0, battery.length - remainingDigits + 1);
+    const y = battery.slice(0, battery.length - remainingDigits);
     for (let i = 0; i < y.length; i++) {
         if (startDigit < battery[i]) {
             startDigit  = battery[i];
@@ -52,16 +52,13 @@ export const maxLargeJoltage = (battery: battery): number  => {
     let [startDigit, index] = findHighestDigitForWidth(currentBattery, 11);
     let digits = [startDigit.toString()];
 
-    for (let i = 11; i > 0; i--) {
+    for (let i = 10; i >= 0; i--) {
         currentBattery = currentBattery.slice(index + 1);
         let [nextDigit, nextIndex] = findHighestDigitForWidth(currentBattery, i);
         index = nextIndex;
         digits.push(nextDigit.toString());
-
-        console.log(digits.join(""), currentBattery.join(""), i);
     }
 
-    console.log(digits.join(""), battery.join(""));
     return parseInt(digits.join(""));
 }
 
@@ -69,9 +66,7 @@ export const maxLargeJoltage = (battery: battery): number  => {
 
 export function solvePart2(input: string): number {
     const batteries = parse(input)
-    console.log(maxLargeJoltage(batteries[0]));
 
-    // return 1;
     return batteries
         .reduce((total, battery) =>
                 total + maxLargeJoltage(battery)
